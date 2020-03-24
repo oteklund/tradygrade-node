017927804b77e40b07d5de684a7cac9063c0ded6
kanlaura <59911290+kanlaura@users.noreply.github.com>
2020-03-24 22:00:17 +0200

    typing message note added to chectSocket

--- chatSocket.js
+++ chatSocket.js
@@ -1,32 +1,37 @@
-var socket_io = require('socket.io');
-var io = socket_io();
-var socketApi = {};
+const io = require('socket.io')();
+
+let connections = []
+let socketApi = {};
+
+
+
+io.on('connection', (socket) => {
+    console.log('Connected', socket.id);
+    socket.on('joinChat', ({user, chatID}) => {
+        socket.join(chatID)
+
+        socket.emit('join', 'Welcome user')
+
+        //User online
+        socket.broadcast.to(chatID).emit('user online', `${user} is online`)
+
+        //Usert typing a message
+        socket.on('typing', (user) => {socket.broadcast.to(chatID).emit('typing', user)})
+    })
+
+    //Listening for chatMessage
+    socket.on('chatMessage', message => {
+        io.emit('new message', message)
+    })
+
+    //User going offline
+    socket.on('disconnect', () => {
+        io.emit('user disconnect', 'user is no longer online')
+        console.log("connection disconnected:", socket.id)
+    })
+
+});
 
 socketApi.io = io;
-
-io.on('connection', function(socket){
-    console.log('Connected', socket.id);
-
-    socket.on("join", (user, room) => {
-        socket.join(room)
-        console.log(`${user} joined room ${room}`)
-        socket.emit("getChatHistory")
-    })
-
-    socket.on('message', (data) => {
-        io.sockets.emit('new message', data);
-    });
-
-    socket.off("disconnect", () => {
-        console.log("user disconnected:", socket.id)
-        const index = connections.findIndex(i => i === socket.id)
-        connections.splice(index, 1)
-    })
-
-    
-    // socket.on('typing', (data) => {
-    //     socket.broadcast.emit('typing', data)
-    // })
-});
 
 module.exports = socketApi; 
