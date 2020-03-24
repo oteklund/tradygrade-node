54318972b098e35429aec26d20de63fe2874b61a
kanlaura <59911290+kanlaura@users.noreply.github.com>
2020-03-24 16:48:20 +0200

    changes to websockets

--- chatSocket.js
+++ chatSocket.js
@@ -7,13 +7,26 @@ socketApi.io = io;
 io.on('connection', function(socket){
     console.log('Connected', socket.id);
 
-    socket.on('chat', (data) => {
-        io.sockets.emit('chat', data);
+    socket.on("join", (user, room) => {
+        socket.join(room)
+        console.log(`${user} joined room ${room}`)
+        socket.emit("getChatHistory")
+    })
+
+    socket.on('message', (data) => {
+        io.sockets.emit('new message', data);
     });
 
-    socket.on('typing', (data) => {
-        socket.broadcast.emit('typing', data)
+    socket.off("disconnect", () => {
+        console.log("user disconnected:", socket.id)
+        const index = connections.findIndex(i => i === socket.id)
+        connections.splice(index, 1)
     })
+
+    
+    // socket.on('typing', (data) => {
+    //     socket.broadcast.emit('typing', data)
+    // })
 });
 
 module.exports = socketApi; 
