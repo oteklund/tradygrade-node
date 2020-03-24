ab3a7675540a8511b8afc225f1d2151c9bdb3b92
kanlaura <59911290+kanlaura@users.noreply.github.com>
2020-03-24 11:23:20 +0200

    chat port added. all porta moved to dotenv

--- sql/schema.sql
+++ sql/schema.sql
@@ -17,7 +17,6 @@ CREATE TABLE IF NOT EXISTS chatter(
     chatter_chat_id INT NOT NULL,
     FOREIGN KEY (chatter_user_id) REFERENCES user(user_id),
     FOREIGN KEY (chatter_chat_id) REFERENCES chat(chat_id));
-)
 
 CREATE TABLE IF NOT EXISTS msg(
 msg_id SERIAL PRIMARY KEY NOT NULL,
@@ -25,5 +24,4 @@ msg_user_id INT NOT NULL,
 msg_chat_id INT NOT NULL,
 msg_timestamp DATE DEFAULT CURRENT_DATE NOT NULL,
     FOREIGN KEY (msg_user_id) REFERENCES user(user_id),
-    FOREIGN KEY (msg_chat_id) REFERENCES chat(chat_id));
-
+    FOREIGN KEY (msg_chat_id) REFERENCES chat(chat_id));§§
