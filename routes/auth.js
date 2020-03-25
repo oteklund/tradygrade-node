5791b29aaa957023f6a3d3ec9c51fa39a91b15c5
oteklund <ottoeklund@gmail.com>
2020-03-25 15:50:12 +0200

    Move auth related HTTP endpoints to auth.js from users.js

--- routes/auth.js
+++ routes/auth.js
@@ -5,13 +5,52 @@
 */
 const express = require('express');
 const router = express.Router();
+const hashService = require("../auth/hashService")
+const jwtService = require("../auth/jwtService")
+const { authenticateToken } = require("./middleware")
+const { getUserByName } = require("../dao/usersDao")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res, next) => {
+    try {
+      const { name, password } = req.body
+      const foundUser = await getUserByName(name)
+      
+      if (foundUser == null) {
+        res.status(400).send("Invalid credentials")
+        return
+      }
+      
+      const passwordIsCorrect = hashService.passwordCompare(password, foundUser.password)
+      if (passwordIsCorrect == null) throw "Error handling login, please try again later"
+      
+      // in case authentication succeeds, we generate and return a jwt + refresh token. refresh tokens are currently stored locally in an array, but will be moved to a db.
+      if (passwordIsCorrect){
+        const token = jwtService.generateToken(foundUser)
+        const refreshToken = jwtService.refreshToken(foundUser)
+        jwtService.refreshTokens.push(refreshToken) // <- move to db
+        res.status(200).json({...foundUser, token: token, refreshToken: refreshToken}) 
+      } else {
+        res.status(400).send("Invalid credentials") 
+      }
+    } catch (err) {
+      res.status(500).send()
+      next(err)
+    }
+  })
 
-})
+  router.delete("/logout", async (req, res, next) => {
+      jwtService.deleteRefreshToken(req.body.refreshToken)
+      res.status(204).send()
+    })
+
+  router.post("/token", async (req, res, next) => {
+      const refreshToken = req.body.refreshToken
+      if (refreshToken == null) res.status(401).send()
+      if (!jwtService.refreshTokens.includes(refreshToken)) res.status(403).send()
+      const newToken = jwtService.verifyRefreshToken(refreshToken)
+      if (newToken == null) res.status(403).send()
+      
+      res.json({ token: newToken })
+    })
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,6 @@ const { getUsers, getUser, createUser, getUserByName } = require('../dao/usersDa
 const { getItemsByUserId } = require('../dao/itemsDao');
 const User = require("../models/User")
 const hashService = require("../auth/hashService")
-const jwtService = require("../auth/jwtService")
 const { authenticateToken } = require("./middleware")
 
 router
@@ -30,55 +29,6 @@ router
       next(err);
     }
   });
-
-router
-  .route("/login")
-  .post(async (req, res, next) => {
-    try {
-      const { name, password } = req.body
-      const foundUser = await getUserByName(name)
-      
-      if (foundUser == null) {
-        res.status(400).send("Invalid credentials")
-        return
-      }
-      
-      const passwordIsCorrect = hashService.passwordCompare(password, foundUser.password)
-      if (passwordIsCorrect == null) throw "Error handling login, please try again later"
-      
-      // in case authentication succeeds, we generate and return a jwt + refresh token. refresh tokens are currently stored locally in an array, but will be moved to a db.
-      if (passwordIsCorrect){
-        const token = jwtService.generateToken(foundUser)
-        const refreshToken = jwtService.refreshToken(foundUser)
-        jwtService.refreshTokens.push(refreshToken) // <- move to db
-        res.status(200).json({...foundUser, token: token, refreshToken: refreshToken}) 
-      } else {
-        res.status(400).send("Invalid credentials") 
-      }
-    } catch (err) {
-      res.status(500).send()
-      next(err)
-    }
-  })
-
-  router
-    .route("/logout")
-    .delete(async (req, res, next) => {
-      jwtService.deleteRefreshToken(req.body.refreshToken)
-      res.status(204).send()
-    })
-
-  router
-    .route("/token")
-    .post(async (req, res, next) => {
-      const refreshToken = req.body.refreshToken
-      if (refreshToken == null) res.status(401).send()
-      if (!jwtService.refreshTokens.includes(refreshToken)) res.status(403).send()
-      const newToken = jwtService.verifyRefreshToken(refreshToken)
-      if (newToken == null) res.status(403).send()
-      
-      res.json({ token: newToken })
-    })
 
 router
   .route('/:id')
