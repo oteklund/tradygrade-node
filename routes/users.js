170db6998de9c26d518fc9758215f2006af628bc
Pekka Härkönen <pekka.o.harkonen@gmail.com>
2020-04-02 13:36:38 +0300

    removed authentication requirement from get one user route temporarily

--- routes/users.js
+++ routes/users.js
@@ -10,12 +10,12 @@ const {
 const { getItemsByUserId } = require('../dao/itemsDao');
 const User = require('../models/User');
 const hashService = require('../auth/hashService');
-const { authenticateToken } = require('../middleware/middleware'); //apply to all routes that require the user to be logged in 
+const { authenticateToken } = require('../middleware/middleware'); //apply to all routes that require the user to be logged in
 
 router
   .route('/')
   // .get(authenticateToken, async (req, res, next) => {
-    .get(async (req, res, next) => {
+  .get(async (req, res, next) => {
     try {
       let usersData = await getUsers();
       if (usersData) {
@@ -65,7 +65,7 @@ router
 
 router
   .route('/:id')
-  .get(authenticateToken, async (req, res, next) => {
+  .get(async (req, res, next) => {
     try {
       let userData = await getUser(req.params.id);
       if (userData) {
